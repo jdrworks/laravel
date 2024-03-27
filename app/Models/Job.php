beeed49d558e4eac499f97749f5ce3a2123d8d47
Jeff Russell <jeff@jdrworks.com>
2024-03-26 23:43:04 -0400

    Episode 11 - Two Key Eloquent Relationship Types Added belongsTo and hasMany relationships between jobs and employers

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
@@ -13,5 +14,10 @@ class Job extends Model {
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
 }
