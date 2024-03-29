71c0cd58141b13c06a05f8af57ad6bdfd777cc0d
Jeff Russell <jeff@jdrworks.com>
2024-03-29 11:00:59 -0400

    Episode 13 - Eager Loading and the N+1 Problem Updated jobs view, eager loading, disabling lazy loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,14 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }}
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
