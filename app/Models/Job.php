087e14ae235c6d3d863c7ac13139dcf28ef0985f
Jeff Russell <jeff@jdrworks.com>
2024-03-28 22:56:46 -0400

    Episode 12 - Pivot Tables and belongsToMany Relationships added tags and pivot table for jobs and tags

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model {
     use HasFactory;
@@ -20,4 +20,8 @@ class Job extends Model {
         return $this->belongsTo(Employer::class);
     }
 
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+    }
 }
