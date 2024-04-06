d43cb71f9a7eaaa268ba615a09d27277d02e7d68
Jeff Russell <jeff@jdrworks.com>
2024-04-05 20:02:00 -0400

    Episode 17 - Always Validate. Never Trust the User. Validation for job create form

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/jobs/create', function () {
 });
 
 Route::post('/jobs', function () {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
 
     Job::create([
        'title' => request('title'),
