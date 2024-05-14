04c3089ac3c6df35daca23f3752668379aa69e25
Jeff Russell <jeff@jdrworks.com>
2024-05-13 23:26:37 -0400

    Episode 30 - The Everything Episode Finishing up the project, adding registration, login, job creation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,33 +8,51 @@
     <title>Pixel Positions</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600;&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600;&display=swap"
+          rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-slate-950 text-white font-hanken-grotesk">
+<body class="bg-slate-950 text-white font-hanken-grotesk pb-20">
 
-    <div class="px-10">
-        <nav class="flex justify-between items-center py-4 border-b border-white/10">
-            <div>
-                <a href="">
-                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
-                </a>
+<div class="px-10">
+    <nav class="flex justify-between items-center py-4 border-b border-white/10">
+        <div>
+            <a href="">
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+            </a>
+        </div>
+        <div class="space-x-6 font-bold">
+            <a href="#">Jobs</a>
+            <a href="#">Careers</a>
+            <a href="#">Salaries</a>
+            <a href="#">Companies</a>
+        </div>
+
+        @auth
+            <div class="space-x-6 font-bold flex">
+                <a href="/jobs/create">Post a Job</a>
+
+                <form method="POST" action="/logout">
+                    @csrf
+                    @method('DELETE')
+
+                    <button>Logout</button>
+                </form>
             </div>
+        @endauth
+
+        @guest
             <div class="space-x-6 font-bold">
-                <a href="#">Jobs</a>
-                <a href="#">Careers</a>
-                <a href="#">Salaries</a>
-                <a href="#">Companies</a>
+                <a href="/register">Sign Up</a>
+                <a href="/login">Login In</a>
             </div>
-            <div>
-                <a href="#">Post a Job</a>
-            </div>
-        </nav>
+        @endguest
+    </nav>
 
-        <main class="mt-10 max-w-[986px] mx-auto">
-            {{ $slot }}
-        </main>
-    </div>
+    <main class="mt-10 max-w-[986px] mx-auto">
+        {{ $slot }}
+    </main>
+</div>
 
 </body>
 </html>
